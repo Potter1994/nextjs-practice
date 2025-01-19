1e22c1a177d8eb2fb9fbcc30d03467f2e13c7761
Potter <sky753159852@gmail.com>
2025-01-20 00:54:27 +0800

    chapter 3 complete

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,12 @@ import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import styles from "./ui/home.module.css";
 import { lusitana } from "@/app/ui/fonts";
+import Image from "next/image";
 
 export default function Page() {
   return (
     <main className='flex min-h-screen flex-col p-6'>
-      <div className={styles.shape} />
+      {/* <div className={styles.shape} /> */}
       <div className='flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52'>
         <AcmeLogo />
       </div>
@@ -29,6 +30,20 @@ export default function Page() {
         </div>
         <div className='flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12'>
           {/* Add Hero Images Here */}
+          <Image
+            src='/hero-desktop.png'
+            alt='Screenshots of the dashboard project showing desktop version'
+            width={1000}
+            height={760}
+            className='hidden md:block'
+          />
+          <Image
+            src='/hero-mobile.png'
+            alt='Screenshots of the dashboard project showing mobile version'
+            width={560}
+            height={620}
+            className='block md:hidden'
+          />
         </div>
       </div>
     </main>
